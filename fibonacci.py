e0566af123ccb3a5671dc39729e6406d608412c7
deepz15dk <deepz15dk@gmail.com>
2026-01-14 14:15:20 +0530

    Fibonacci series in Python
    This Python program generates the Fibonacci series based on the number of terms entered by the user. It uses a loop to calculate and display each number in the series.

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+# Fibonacci series program
+
+n = int(input("Enter number of terms: "))
+
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a)
+    c = a + b
+    a = b
+    b = c
